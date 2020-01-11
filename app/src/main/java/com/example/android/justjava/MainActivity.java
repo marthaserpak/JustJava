10d17af3ee48ef823ecbb3597032666d95eef930
martha <m.v.serpak@gmail.com>
2020-01-11 13:02:55 +0200

    0

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private int quantity;
-    private int price;
+    private CheckBox whippedCream;
+    private CheckBox chocolate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        whippedCream = findViewById(R.id.whippedCream);
+        chocolate = findViewById(R.id.chocolate);
     }
 
     public void submitOrder(View view) {
@@ -26,9 +31,20 @@ public class MainActivity extends AppCompatActivity {
     private String createOrderSummary(int price) {
         String priceMassage =
                 "Name: Marta";
-        priceMassage += priceMassage + "\nQuantitty: " + quantity;
-        priceMassage += priceMassage + "\nTotal price: " + "$" + price;
-        priceMassage += priceMassage + "\nThank you!";
+        priceMassage = priceMassage + "\nToppings: ";
+        if (whippedCream.isChecked()) {
+            priceMassage = priceMassage + "\n\tAdd whipped cream";
+        } else {
+            priceMassage = priceMassage + "\n\tWithout whipped cream";
+        }
+        if(chocolate.isChecked()){
+            priceMassage = priceMassage + "\n\tWith chocolate";
+        } else {
+            priceMassage = priceMassage + "\n\tWithout chocolate";
+        }
+        priceMassage = priceMassage + "\nQuantitty: " + quantity;
+        priceMassage = priceMassage + "\nTotal price: " + "$" + price;
+        priceMassage = priceMassage + "\nThank you!";
 
         return priceMassage;
     }
